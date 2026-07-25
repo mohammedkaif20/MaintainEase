8b173bec14b30e23842af44442301749b6d20db4
Mohammed Kaif <iqbalkaif8@gmail.com>
2026-07-25 14:24:46 +0530

    Configure app for Render deployment

--- server/app.js
+++ server/app.js
@@ -18,14 +18,14 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use('/api/complaints', complaintRoutes);
 
 // Connect to MongoDB and start server
-const MONGO_URI = 'mongodb://localhost:27017/maintainease';
-const PORT = 3000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/maintainease';
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
